c2bf78099581436df10680f8fa1c5bb53969f220
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-01-08 17:59:17 -0200

    Realização do exercício inerente a Classes.

---
+++ Training_CSharp/Grades/Model/GradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Grades.Model
+{
+    public class GradeBook
+    {
+        public void AddGrade(float grade)
+        {
+           grades.Add(grade);
+        }
+
+        List<float> grades;
+    }
+}
---
+++ Training_CSharp/Grades/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Grades
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
