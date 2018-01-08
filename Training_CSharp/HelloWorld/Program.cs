f8e091c4172feb0d8c106863f953fa965d6a22a0
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-01-08 17:18:39 -0200

    first commit.

---
+++ Training_CSharp/HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o seu nome.....: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Quantas horas você dormiu na noite passada?!");
+            int horasDormidas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Olá " + nome);
+
+            if (horasDormidas > 8)
+            {
+                Console.WriteLine("Então você dormiu bem!!");
+            }
+            else
+            {
+                Console.WriteLine("Você precisa descansar mais!!!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
