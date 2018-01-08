61577ea9a8c8709ec8e880c43f6f4d94f9f7220d
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-01-08 20:11:31 -0200

    Exercício sobre uso de constructors.

--- Training_CSharp/Grades/Model/GradeBook.cs
+++ Training_CSharp/Grades/Model/GradeBook.cs
@@ -4,11 +4,17 @@ namespace Grades.Model
 {
     public class GradeBook
     {
+
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
         public void AddGrade(float grade)
         {
            grades.Add(grade);
         }
 
-        List<float> grades;
+        List<float> grades = new List<float>();
     }
 }
--- Training_CSharp/Grades/Program.cs
+++ Training_CSharp/Grades/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace Grades
+﻿using System;
+using Grades.Model;
+
+namespace Grades
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            var book = new GradeBook();
+
+            book.AddGrade(91);
+            book.AddGrade(89.9f);
+
+            Console.ReadKey();
         }
     }
 }
